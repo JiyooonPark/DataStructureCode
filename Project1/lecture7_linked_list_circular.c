8ee14f06de7f6094665c7234df468d726e62c90c
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-10-13 18:44:23 +0900

    lecture 7 basics

---
+++ Project1/lecture7_linked_list_circular.c
@@ -0,0 +1,72 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//typedef char element[100];
+//typedef struct ListNode {
+//	element data;
+//	struct ListNode* link;
+//}ListNode;
+//
+//void print_list(ListNode* head) {
+//	ListNode* p = head;
+//	 do{
+//		printf("<- %s ->", p->data);
+//		p = p->link;
+//	}while (p != head);
+//	printf("\n");
+//}
+//
+//ListNode* insert_first(ListNode* head, element data) {
+//	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+//	strcpy_s(node->data,100, data);
+//	if (head == NULL) {
+//		head = node;
+//		node->link = head;
+//	}
+//	else {
+//		node->link = head->link;
+//		head->link = node;
+//	}
+//	//print_list(head);
+//	return head;
+//}
+//ListNode* insert_last(ListNode* head, element data) {
+//	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+//	strcpy_s(node->data, 100, data);
+//	if (head == NULL) {
+//		head = node;
+//		node->link = head;
+//	}
+//	else {
+//		node->link = head->link;
+//		head->link = node;
+//		head = node;
+//	}
+//	print_list(head);
+//	return head;
+//}
+//
+//int main() {
+//	ListNode* head = NULL;
+//	//head = insert_first(head, "hello");
+//	//head = insert_first(head, "my");
+//	//head = insert_first(head, "name");
+//	//head = insert_first(head, "is");
+//	//head = insert_first(head, "jypark");
+//
+//	//head = insert_last(head, "20");
+//	//head = insert_last(head, "30");
+//	//head = insert_last(head, "40");
+//	//head = insert_last(head, "50");
+//	//head = insert_last(head, "10");
+//
+//	head = insert_first(head, "KIM");
+//	head = insert_first(head, "PARK");
+//	head = insert_first(head, "SHIN");
+//
+//	for (int i = 0; i < 10; i++) {
+//		printf("now : %s\n", head->data);
+//		head = head->link;
+//	}
+//}
---
+++ Project1/lecture7_linked_list_double.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int element;
+typedef struct DlistNode {
+	element data;
+	struct DlistNode* rlink, * llink;
+}DlistNode;
+
+void dinsert(DlistNode* before, element data) {
+	DlistNode* newnode = (DlistNode*)malloc(sizeof(DlistNode));
+	newnode->data = data;
+
+
+	newnode->llink = before;
+	newnode->rlink = before->rlink;
+	
+	before->rlink->llink = newnode;
+	before->rlink = newnode;
+}
+void ddelete(DlistNode* head, DlistNode* removed) {
+	if (removed == head) return;
+	removed->llink->rlink = removed->rlink;
+	removed->rlink->llink = removed->llink;
+	free(removed);
+}
+void init(DlistNode* phead) {
+	phead->llink = phead;
+	phead->rlink = phead;
+}
+void print_dlist(DlistNode* phead) {
+	DlistNode* p;
+	for (p = phead->rlink; p != phead; p = p->rlink) {
+		printf("<- |%d| ->", p->data);
+	}
+	printf("\n");
+}
+int main() {
+	DlistNode* head = (DlistNode*)malloc(sizeof(DlistNode));
+	init(head);
+	printf("추가 단계\n");
+	for (int i = 0; i < 5; i++) {
+		dinsert(head, i);
+		print_dlist(head);
+	}
+	printf("삭제 단계\n");
+	for (int i = 0; i < 5; i++) {
+		print_dlist(head);
+		ddelete(head, head->rlink);
+	}
+	free(head);
+	return 0;
+}
