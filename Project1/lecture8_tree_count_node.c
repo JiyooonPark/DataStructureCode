a1a24da4792edfcc8eb584400388fd3b53a7e641
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-10-17 09:36:17 +0900

    tree node count, tree height, tree leaf count

---
+++ Project1/lecture8_tree_count_node.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct TreeNode {
+	int data;
+	struct TreeNode* right, * left;
+}TreeNode;
+
+int get_node_count(TreeNode* node) {
+	int count = 0;
+	if (node != NULL) {
+		count = 1 + get_node_count(node->right) + get_node_count(node->left);
+	}
+	return count;
+}
+int get_height(TreeNode* node) {
+	int height = 0;
+	if (node != NULL) {
+		height = 1 + max(get_height(node->left), get_height(node->right));
+	}
+}
